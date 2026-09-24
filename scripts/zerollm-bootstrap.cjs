d2dea587070ec67a1c57c55fa615c177a8f50032
Travelintrips <divatranssoetta@gmail.com>
2026-09-25 02:20:43 +0700

    chore(zerollm): remove unused os import

--- scripts/zerollm-bootstrap.cjs
+++ scripts/zerollm-bootstrap.cjs
@@ -1,5 +1,4 @@
 const fs = require("node:fs");
-const os = require("node:os");
 const path = require("node:path");
 const crypto = require("node:crypto");
 const { spawnSync } = require("node:child_process");
